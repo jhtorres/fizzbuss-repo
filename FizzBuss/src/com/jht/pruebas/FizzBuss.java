ffa4ab8a3f7f88d1a063207722acb186ee1af050
Jose Torres <jtorres@redsis.com>
2013-04-20 09:11:32 -0500

    Primer commit

---
+++ FizzBuss/src/com/jht/pruebas/FizzBuss.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.jht.pruebas;
+
+/**
+ * @author jtorres
+ * Write a program that prints the numbers from 1 to 100. 
+ * But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
+ * For numbers which are multiples of both three and five print "FizzBuzz".
+ */
+public class FizzBuss {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i=1; i <= 100; i++) {
+			boolean mul3 = false;
+			boolean mul5 = false;
+			if (i%3 == 0) {
+				mul3 = true;
+			}
+			if (i%5 == 0) {
+				mul5 = true;
+			} 
+			if (mul3 && mul5) {
+				System.out.println("FizzBuzz");
+			} else if (mul3) {
+				System.out.println("Fizz");
+			} else if (mul5) {
+				System.out.println("Buzz");
+			} else 
+				System.out.println("i=" + i);
+		}
+
+	}
+
+}
